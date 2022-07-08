22088661109105e3d6dae2eedf0520026a848877
NeilClack <c.neilclack@gmail.com>
2022-07-07 23:32:03 -0500

    Sucessfully extract all models from Printables. Names and URLs

--- threedprinting/threedprinting/spiders/model_spider.py
+++ threedprinting/threedprinting/spiders/model_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy_selenium import SeleniumRequest
+from bs4 import BeautifulSoup
 from datetime import datetime
 
 
@@ -16,18 +17,45 @@ class ModelSpier(scrapy.Spider):
 
         for url in urls:
             yield SeleniumRequest(
-                url = url,
-                wait_time = 20,
-                screenshot = True,
-                callback = self.parse,
-                dont_filter = True
+                url=url,
+                wait_time=20,
+                screenshot=True,
+                callback=self.parse,
+                dont_filter=True,
             )
 
     def parse(self, response):
         """Parses the request responses, extracts model information and sends that information to process_data() for storage."""
 
+        # The url of the page, to be used later
         page = response.url
+
+        # Creating soup from the response body
+        soup = BeautifulSoup(response.body, "html.parser")
+
+        # finding all print-card tags - returns a resultset
+        tags = soup.find_all("print-card")
+
+        # Iterating over the tags to get the URL and model name of each tag.
+        for tag in tags:
+            soup = tag
+            # locate the a tag
+            atag = soup.h3.a
+
+            # Model Name
+            name = atag.contents
+
+            # Model URL
+            url_slug = atag["href"]
+            url = f"https://www.printables.com{url_slug}"
+
+        # Creating filename, will use URL later
         filename = f"models-printables.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)
+
+        # Open file and write tags.
+        with open(filename, "w", encoding="utf-8") as f:
+            tags = list(str(tags))
+            for tag in tags:
+                f.write(tag)
+
         self.log(f"Saved file {filename} at {datetime.now()}")
